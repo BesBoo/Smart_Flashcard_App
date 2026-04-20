8d87630706ef1db7cab02ba806efcd606d3920e7
BunBo. K_hanh <tranduyduc9679@gmail.com>
2026-04-20 13:02:55 +0700

    Add EF migration for IsNotifiedToOwner column

---
+++ backend/SmartFlashcardAPI/Migrations/20260420060211_AddIsNotifiedToOwner.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartFlashcardAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsNotifiedToOwner : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsNotifiedToOwner",
+                table: "ContentReports",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsNotifiedToOwner",
+                table: "ContentReports");
+        }
+    }
+}
